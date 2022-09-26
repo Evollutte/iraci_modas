0af5b8e6b58eacd689e7c55314a790c25b5a5e8d
Evollutte <gersonjunior100@gmail.com>
2022-09-25 21:54:37 -0300

    banner

--- apps/banner/models.py
+++ apps/banner/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Banner(models.Model):
   name = models.CharField(max_length=120, verbose_name='Nome', unique=True)
-  image_url = models.URLField(max_length=1024, blank=True, null=True)
+  # image_url = models.URLField(max_length=1024, blank=True, null=True)
   image = models.ImageField(upload_to='banner/')
 
   def __str__(self):
